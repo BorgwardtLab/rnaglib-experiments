a6f81efbe54fe2a44b1bf77ae67b23e0e5bd7d25
cgoliver <carlos.gonzalez.oliver@gmail.com>
2025-01-30 20:03:07 -0500

    lit plot

--- make_table.py
+++ make_table.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-TASKLIST = ["rna_cm", "rna_go", "rna_ligand", "rna_prot", "rna_site", "rna_if", "rna_site_bench"]
+TASKLIST = ["rna_cm", "rna_go", "rna_ligand", "rna_prot", "rna_site", "rna_if"]
 RNAFM = [True, False]
 DISTANCES = ["USalign", "cd_hit"]
 SEEDS = [0, 1, 2]
@@ -16,7 +16,6 @@ METRICS = {
     "rna_prot": "mcc",
     "rna_site": "auroc",
     "rna_if": "accuracy",
-    "rna_site_bench": "accuracy",
 }
 
 rows = []
@@ -72,4 +71,67 @@ for var in ["gnn_layers", "distance", "rnafm"]:
     g.set(ylim=(0, 1))
     g.despine(left=True)
     plt.savefig(var + ".pdf", format="pdf")
-    plt.show()
+    plt.clf()
+
+## LITERATURE
+
+TASKLIST = ["rna_site_bench"]
+RNAFM = [True, False]
+SEEDS = [0, 1, 2]
+LAYERS = [0, 1, 2]
+
+METRICS = {
+    "rna_site_bench": "accuracy",
+}
+
+rows = []
+for task in TASKLIST:
+    for rnafm in RNAFM:
+        for seed in SEEDS:
+            for layer in LAYERS:
+                try:
+                    with open(f"results/{task}_rnafm-{rnafm}_layers-{layer}_seed-{seed}_results.json") as result:
+                        result = json.load(result)["test_metrics"]
+                        rows.append(
+                            {
+                                "score": result[METRICS[task]],
+                                "metric": METRICS[task],
+                                "task": task,
+                                "seed": seed,
+                                "gnn_layers": layer,
+                                "distance": distance,
+                                "rnafm": rnafm,
+                            }
+                        )
+                except FileNotFoundError:
+                    continue
+                pass
+pass
+
+df = pd.DataFrame(rows)
+df_mean = df.groupby(["task", "gnn_layers", "distance", "rnafm"])["score"].mean().reset_index()
+df_std = df.groupby(["task", "gnn_layers", "distance", "rnafm"])["score"].std().reset_index()
+df_mean["std"] = df_std["score"]
+df_mean["metric"] = [METRICS[row.task] for row in df_mean.itertuples()]
+
+df_mean.to_csv("benchmark_results.csv")
+print(df_mean)
+
+for var in ["gnn_layers", "rnafm"]:
+    g = sns.catplot(
+        data=df_mean,
+        x=var,
+        y="score",
+        col="task",
+        hue=var,
+        kind="bar",
+        height=4,
+        aspect=0.6,
+    )
+    g.set_axis_labels("", "Test Score")
+    # g.set_xticklabels(["Men", "Women", "Children"])
+    # g.set_titles("{col_name} {col_var}")
+    g.set(ylim=(0, 1))
+    g.despine(left=True)
+    plt.savefig(var + "_lit.pdf", format="pdf")
+    plt.clf()
