53c81a243ae9bb7cd2b848a2168c15e1037ef6f8
cgoliver <carlos.gonzalez.oliver@gmail.com>
2025-03-18 16:00:12 -0500

    rna prot

--- base/RNA_PROT_exp.py
+++ base/RNA_PROT_exp.py
@@ -7,50 +7,30 @@ import shutil
 import torch
 
 from rnaglib.learning.task_models import PygModel
-from rnaglib.tasks import ProteinBindingSite
+from rnaglib.tasks import get_task 
 from rnaglib.transforms import GraphRepresentation
-from rnaglib.dataset_transforms import CDHitComputer, ClusterSplitter, StructureDistanceComputer
 
-STRUC_ROOT = "roots/RNA_RBP_struc"
-SEQ_ROOT = "roots/RNA_RBP_seq"
+from exp import RNATrainer
 
-# Setup task
-ta_RBP_struc = ProteinBindingSite(STRUC_ROOT, recompute=False, debug=False)
-ta_RBP_struc.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta = get_task(root="roots/RNA_PROT", task_id="rna_prot")
 
-if os.path.exists(SEQ_ROOT):
-    ta_RBP_seq = ProteinBindingSite(
-        root=SEQ_ROOT,
-        recompute=False,
-        debug=False,
-    )
-    ta_RBP_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-else:
-    ta_RBP_seq = ProteinBindingSite(
-        root=SEQ_ROOT,
-        recompute=False,
-        debug=False,
-    )
-    distance = CDHitComputer()
-    ta_RBP_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-    ta_RBP_seq.dataset = distance(ta_RBP_seq.dataset)
-
-    ta_RBP_seq.splitter = ClusterSplitter(distance_name=distance.name)
-
-    ta_RBP_seq.set_loaders(recompute=True)
-
-    source = SEQ_ROOT
-    tmp = "roots/RNA_RBP_tmp"
-
-    shutil.copytree(source, tmp, dirs_exist_ok=True)
-    shutil.rmtree(source)
-    os.rename(tmp, source)
-
-
-
-model_RBP = {
-        "num_node_features": ta_RBP_seq.metadata["description"]["num_node_features"],
-        "num_classes": ta_RBP_seq.metadata["description"]["num_classes"],
+model_args = {
+        "num_node_features": ta.metadata["num_node_features"],
+        "num_classes": ta.metadata["num_classes"],
         "graph_level": False,
         "num_layers": 3,
 }
+
+
+# Setup task
+ta.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta.get_split_loaders()
+
+model = PygModel(**model_args)
+trainer = RNATrainer(ta, model)
+
+trainer.train()
+
+if __name__ == "__main__":
+    pass
+
