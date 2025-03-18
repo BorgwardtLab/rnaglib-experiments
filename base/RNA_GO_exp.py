a6ade2bd0426db33124fa49f77a4342765163446
cgoliver <carlos.gonzalez.oliver@gmail.com>
2025-03-18 13:21:11 -0500

    if trains

--- base/RNA_GO_exp.py
+++ base/RNA_GO_exp.py
@@ -7,51 +7,32 @@ import shutil
 import torch
 
 from rnaglib.learning.task_models import PygModel
-from rnaglib.tasks import RNAGo
+from rnaglib.tasks import get_task
 from rnaglib.transforms import GraphRepresentation
 from rnaglib.dataset_transforms import CDHitComputer, ClusterSplitter, StructureDistanceComputer
+from exp import RNATrainer
 
-
-SEQ_ROOT = "roots/RNA_GO_seq"
-STRUC_ROOT = "roots/RNA_GO_struc"
 
 # Setup task
-ta_GO_seq = RNAGo(root=SEQ_ROOT, recompute=False, debug=False)
-ta_GO_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-
-if os.path.exists(STRUC_ROOT):
-    ta_GO_struc = RNAGo(
-        root=STRUC_ROOT,
-        recompute=False,
-        debug=False,
-    )
-    ta_GO_struc.dataset.add_representation(GraphRepresentation(framework="pyg"))
-else:
-    ta_GO_struc = RNAGo(
-        root=STRUC_ROOT,
-        recompute=False,
-        debug=False,
-    )
-    distance = StructureDistanceComputer()
-    ta_GO_struc.dataset.add_representation(GraphRepresentation(framework="pyg"))
-    ta_GO_struc.dataset = distance(ta_GO_seq.dataset)
-
-    ta_GO_struc.splitter = ClusterSplitter(distance_name=distance.name)
-
-    ta_GO_struc.set_loaders(recompute=True)
-    ta_GO_struc.write()
-
-    source = STRUC_ROOT
-    tmp = "roots/RNA_GO_tmp"
-
-    shutil.copytree(source, tmp, dirs_exist_ok=True)
-    shutil.rmtree(source)
-    os.rename(tmp, source)
+ta = get_task(root="roots/RNA_GO", task_id="rna_go")
 # Create model
-model_GO = {
-        "num_node_features": ta_GO_seq.metadata["description"]["num_node_features"],
-        "num_classes": len(ta_GO_seq.metadata["label_mapping"]),
+model_args = {
+        "num_node_features": ta.metadata["num_node_features"],
+        "num_classes": len(ta.metadata["label_mapping"]),
         "graph_level": True,
         "multi_label": True,
         "num_layers": 3,
     }
+
+
+
+ta.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta.get_split_loaders()
+
+model = PygModel(**model_args)
+trainer = RNATrainer(ta, model)
+
+trainer.train()
+
+if __name__ == "__main__":
+    pass
--- base/RNA_IF_exp.py
+++ base/RNA_IF_exp.py
@@ -7,42 +7,26 @@ import shutil
 import torch
 
 from rnaglib.learning.task_models import PygModel
-from rnaglib.tasks import InverseFolding
+from rnaglib.tasks import get_task 
 from rnaglib.transforms import GraphRepresentation
-from rnaglib.dataset_transforms import CDHitComputer, ClusterSplitter, StructureDistanceComputer
 
-SEQ_ROOT = "roots/RNA_IF_seq"
-STRUC_ROOT = "roots/RNA_IF_struc"
+from exp import RNATrainer
 
-# Setup task
-ta_IF_struc = InverseFolding(root=STRUC_ROOT, recompute=False, debug=False)
-ta_IF_struc.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta = get_task(root="roots/RNA_IF", task_id="rna_if")
+ta.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta.get_split_loaders()
 
-if os.path.exists(SEQ_ROOT):
-    ta_IF_seq = InverseFolding(root=SEQ_ROOT, recompute=False, debug=False)
-    ta_IF_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-else:
-    ta_IF_seq = InverseFolding(root=SEQ_ROOT, recompute=False, debug=False)
-    distance = CDHitComputer()
-    ta_IF_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-    ta_IF_seq.dataset = distance(ta_IF_seq.dataset)
-
-    ta_IF_seq.splitter = ClusterSplitter(distance_name=distance.name)
-
-    ta_IF_seq.set_loaders(recompute=True)
-
-    source = SEQ_ROOT
-    tmp = "roots/RNA_IF_tmp"
-
-    shutil.copytree(source, tmp, dirs_exist_ok=True)
-    shutil.rmtree(source)
-    os.rename(tmp, source)
-
-
-# Create model
-model_IF =   {
-        "num_node_features": ta_IF_seq.metadata["description"]["num_node_features"],
-        "num_classes": ta_IF_seq.metadata["description"]["num_classes"] + 1,
+model_args  =   {
+        "num_node_features": ta.metadata["num_node_features"],
+        "num_classes": ta.metadata["num_classes"] + 1,
         "graph_level": False,
         "num_layers": 3,
     }
+model = PygModel(**model_args)
+trainer = RNATrainer(ta, model)
+
+trainer.train()
+
+
+# Create model
+
