3b70b4a4003f512c9c558685788e5d2cbd9bc8fc
wisskarrou <wisskarrou@gmail.com>
2025-03-24 09:44:45 +0100

    set post-gridsearch hyperparameters for RNA_Site

--- base/RNA_SITE_exp.py
+++ base/RNA_SITE_exp.py
@@ -5,50 +5,31 @@ import sys
 import shutil
 
 import torch
-from rnaglib.learning.task_models import PygModel
-from rnaglib.tasks import BindingSite
-from rnaglib.transforms import GraphRepresentation
-from rnaglib.dataset_transforms import CDHitComputer, ClusterSplitter, StructureDistanceComputer
 
-SEQ_ROOT = "roots/RNA_SITE_seq"
-STRUC_ROOT = "roots/RNA_SITE_struc"
+from rnaglib.learning.task_models import PygModel
+from rnaglib.tasks import get_task
+from rnaglib.transforms import GraphRepresentation
+
+from exp import RNATrainer
 
 # Setup task
-ta_SITE_struc = BindingSite(root=STRUC_ROOT, debug=False, recompute=False)
-ta_SITE_struc.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta = get_task(root="roots/RNA_Site", task_id="rna_site")
 
-if os.path.exists(SEQ_ROOT):
-    ta_SITE_seq = BindingSite(
-        root=SEQ_ROOT,
-        recompute=False,
-        debug=False,
-    )
-    ta_SITE_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-else:
-    ta_SITE_seq = BindingSite(
-        root=SEQ_ROOT,
-        recompute=False,
-        debug=False,
-    )
-    distance = CDHitComputer()
-    ta_SITE_seq.dataset.add_representation(GraphRepresentation(framework="pyg"))
-    ta_SITE_seq.dataset = distance(ta_SITE_seq.dataset)
+ta.dataset.add_representation(GraphRepresentation(framework="pyg"))
+ta.get_split_loaders(batch_size=8)
 
-    ta_SITE_seq.splitter = ClusterSplitter(distance_name=distance.name)
-
-    ta_SITE_seq.set_loaders(recompute=True)
-
-    source = SEQ_ROOT
-    tmp = "roots/RNA_SITE_tmp"
-
-    shutil.copytree(source, tmp, dirs_exist_ok=True)
-    shutil.rmtree(source)
-    os.rename(tmp, source)
-
-# Create model
-model_SITE = {
-        "num_node_features": ta_SITE_seq.metadata["description"]["num_node_features"],
-        "num_classes": ta_SITE_seq.metadata["description"]["num_classes"],
-        "graph_level": False,
-        "num_layers": 3,
+model_args = {
+    "num_node_features": ta.metadata["num_node_features"],
+    "num_classes": ta.metadata["num_classes"],
+    "graph_level": False,
+    "num_layers": 4,
+    "hidden_channels": 256
 }
+
+model = PygModel(**model_args)
+trainer = RNATrainer(ta, model, epochs=100)
+
+trainer.train()
+
+if __name__ == "__main__":
+    pass
