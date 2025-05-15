25606b9568c294ef88a996f03b0683c55177c910
Vincentx15 <vincent.mallet96@gmail.com>
2025-05-15 19:06:02 +0200

    Minor adjustments

--- plotting_scripts/make_table_thresholds.py
+++ plotting_scripts/make_table_thresholds.py
@@ -96,12 +96,29 @@ ax = sns.lineplot(
 
 # Set axis labels and title
 plt.xlabel("Threshold")
-plt.ylabel("Score")
+plt.ylabel("Test Score")
 plt.tight_layout()
+
+
+# Create handles and labels manually
+handles = []
+labels = []
+for i, distance in enumerate(dist_names.values()):
+    # Create a dummy rectangle for each distance, using the color from the plot
+    color = palette_dict[distance]  # Get color for this distance
+    # color = sns.color_palette()[i]  # Get color for this distance
+    # handle = mpatches.Circle((0, 0), radius=0.5, color=color)
+    # Using mlines.Line2D with marker 'o'
+    handle = mlines.Line2D([], [], color=color, marker='o', linestyle='None', markersize=10, )
+    # label=distance_name) # Use Line2D with marker 'o' and set markersize
+    # handle = plt.Rectangle((0, 0), 1, 1, color=color)  # Create a rectangle with that color
+    handles.append(handle)
+    labels.append(distance)
+plt.legend(handles, labels, loc="upper center", ncol=3, title=r"Splitting strategy:", handletextpad=-0.3)
 
 # # Format axes
 # g.set_axis_labels("Threshold", "Test Score")
-# g.set(ylim=(0.5, 0.85))
+ax.set(ylim=(0.5, 1))
 sns.despine()
 #
 # # Save + show
