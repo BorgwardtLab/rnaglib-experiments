623bdf6fbcecdf5c096238e6a1dfe7eb1658aac6
Vincentx15 <vincent.mallet96@gmail.com>
2025-07-28 20:21:23 +0200

    update timing script a bit

--- timing.py
+++ timing.py
@@ -20,45 +20,48 @@ TASKS = [
 
 ]
 
-os.makedirs("timing", exist_ok=True)
-device = "cuda"
+resdir = "timing_exp"
+os.makedirs(resdir, exist_ok=True)
 rows = []
-for task in TASKS:
-    print()
-    print(f'Timing for {task.name}')
-    epochs = 1 if task.name == "rna_if" else 3
-    batch_size = 8
+for device in ("cpu", "cuda"):
+    if device == "cuda" and not torch.cuda.is_available():
+        continue
+    for task in TASKS:
+        print()
+        print(f'Timing for {task.name}')
+        epochs = 1 if task.name == "rna_if" else 3
+        batch_size = 8
 
-    # Inverse folding is slower, so we should not do 3 whole epochs
-    ta = task(root=os.path.join("timing", task.name))
-    ta.add_representation(GraphRepresentation(framework="pyg"))
-    ta.get_split_loaders(batch_size=batch_size)
-    model = PygModel.from_task(ta, device=device) if ta.name != "rna_vs" else VSPygModel.from_task(ta, device=device)
-    model.configure_training(learning_rate=0.001)
+        # Inverse folding is slower, so we should not do 3 whole epochs
+        ta = task(root=os.path.join(resdir, task.name))
+        ta.add_representation(GraphRepresentation(framework="pyg"))
+        ta.get_split_loaders(batch_size=batch_size)
+        model = PygModel.from_task(ta, device=device) if ta.name != "rna_vs" else VSPygModel.from_task(ta,
+                                                                                                       device=device)
+        model.configure_training(learning_rate=0.001)
 
-    # Now time and monitor training
-    tic = time.time()
-    if device != "cpu":
-        torch.cuda.reset_peak_memory_stats(device)
+        # Now time and monitor training
+        tic = time.time()
+        if device != "cpu":
+            torch.cuda.reset_peak_memory_stats(device)
 
-    model.train_model(ta, epochs=epochs)
+        model.train_model(ta, epochs=epochs)
 
-    if device != "cpu":
-        # Get memory and convert to megabytes (MB) for readability
-        peak_memory_bytes = torch.cuda.max_memory_allocated(device)
-        peak_memory_mb = peak_memory_bytes / (1024 * 1024)
-    else:
-        peak_memory_mb = 0
+        if device != "cpu":
+            # Get memory and convert to megabytes (MB) for readability
+            peak_memory_bytes = torch.cuda.max_memory_allocated(device)
+            peak_memory_mb = peak_memory_bytes / (1024 * 1024)
+        else:
+            peak_memory_mb = 0
 
-    time_training = time.time() - tic
-    time_per_point = time_training / len(ta.train_dataset)
-    print(f"Time to train model over 3 epochs: {time_training:.3f} seconds, "
-          f"time per point: {time_per_point:.4f} seconds,"
-          f" memory used: {peak_memory_mb:.2f} MB")
-    rows.append({"task_name": task.name,
-                 "time": time_per_point,
-                 "memory": peak_memory_mb
-                 })
+        time_training = time.time() - tic
+        time_per_point = time_training / len(ta.train_dataset)
+        print(f"Time to train {task.name} model on {device} over 3 epochs: {time_training:.3f} seconds, "
+              f"time per point: {time_per_point:.4f} seconds,"
+              f" memory used: {peak_memory_mb:.2f} MB")
+        rows.append({"task_name": task.name,
+                     "time": time_per_point,
+                     "memory": peak_memory_mb
+                     })
 df = pd.DataFrame(rows)
-
 print(df)
